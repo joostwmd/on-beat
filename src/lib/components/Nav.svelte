3cce919c168e5f1923c6626861655278ed99b949
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-06-13 16:21:12 +0200

    nested navigation

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	export let closeDrawer: () => void;
+
+	$: classesActive = (href: string) =>
+		href === $page.url.pathname ? '!variant-filled-primary' : '';
+</script>
+
+<nav class="list-nav p-4">
+	<ul>
+		<li>
+			<a on:click={closeDrawer} href="/newplaylist" class={classesActive('/newplaylist')}
+				>New Playlist</a
+			>
+		</li>
+		<li>
+			<a on:click={closeDrawer} href="/filterplaylist" class={classesActive('/filterplaylist')}
+				>Filter Playlist</a
+			>
+		</li>
+	</ul>
+</nav>
